96d2449331c7f76a6efbbafcf2d85346c54c94fd
Olha Bahno <obahn@softserveinc.com>
2019-02-04 13:53:40 +0200

    with changes

--- tests/tests_for_cat.rb
+++ tests/tests_for_cat.rb
@@ -1,26 +1,28 @@
 require 'rspec'
-require './cat'
+require './cat.rb'
 require './random_name'
 describe Cat do
 
+  before (:context) do
+    @cat1 = Cat.new (random)
+  end
+
   it "should return 6" do
-    $cat1 = Cat.new(random)
-    expect($cat1.age).to eq 6
+    @cat1 = Cat.new(random)
+    expect(@cat1.age).to eq 6
   end
 
   it "should return 7" do
-    $cat2 = Cat.new(random)
-    expect($cat2.age).to eq 7
+    @cat1 = Cat.new(random)
+    expect(@cat1.age).to eq 7
   end
 
   it "should return 8" do
-    $cat3 = Cat.new(random)
-    expect($cat3.age).to eq 8
+    @cat1 = Cat.new(random)
+    expect(@cat1.age).to eq 8
   end
 
   after(:all) do
-    puts "The first dog: #{$cat1.greeting}"
-    puts "The first dog: #{$cat2.greeting}"
-    puts "The first dog: #{$cat3.greeting}"
+    puts "The first dog: #{@cat1.greeting}"
   end
 end
