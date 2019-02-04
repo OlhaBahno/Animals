15879457579d91cf07ec5d9484604ed0be2e3044
Olha Bahno <obahn@softserveinc.com>
2019-02-04 14:17:56 +0200

    new branch - new stash

--- tests/tests_for_cat.rb
+++ tests/tests_for_cat.rb
@@ -4,25 +4,25 @@ require './random_name'
 describe Cat do
 
   before (:context) do
-    @cat1 = Cat.new (random)
+    @cat = Cat.new (random)
   end
 
   it "should return 6" do
-    @cat1 = Cat.new(random)
-    expect(@cat1.age).to eq 6
+    @cat = Cat.new(random)
+    expect(@cat.age).to eq 6
   end
 
   it "should return 7" do
-    @cat1 = Cat.new(random)
-    expect(@cat1.age).to eq 7
+    @cat = Cat.new(random)
+    expect(@cat.age).to eq 7
   end
 
   it "should return 8" do
-    @cat1 = Cat.new(random)
-    expect(@cat1.age).to eq 8
+    @cat = Cat.new(random)
+    expect(@cat.age).to eq 8
   end
 
   after(:all) do
-    puts "The first dog: #{@cat1.greeting}"
+    puts "The first dog: #{@cat.greeting}"
   end
 end
--- tests/tests_for_dog.rb
+++ tests/tests_for_dog.rb
@@ -4,37 +4,37 @@ require './random_name'
 describe Dog do
 
   before (:context) do
-    @dog1 = Dog.new(random)
+    @dog = Dog.new(random)
     @dog2 = Dog.new(random)
   end
 
   it "should return 2" do
     @dog2 = Dog.new(random)
-    expect(@dog1.age+@dog2.age).to eq 2
+    expect(@dog.age+@dog2.age).to eq 2
   end
 
   it "should return 3" do
     @dog2 = Dog.new(random)
-    expect(@dog1.age+@dog2.age).to eq 3
+    expect(@dog.age+@dog2.age).to eq 3
   end
 
   it "should return 4" do
     @dog2 = Dog.new(random)
-    expect(@dog1.age+ @dog2.age).to eq 4
+    expect(@dog.age+ @dog2.age).to eq 4
   end
 
   it "should return 5" do
     @dog2 = Dog.new(random)
-    expect(@dog1.age+@dog2.age).to eq 5
+    expect(@dog.age+@dog2.age).to eq 5
   end
 
   it "should return 6" do
     @dog2 = Dog.new(random)
-    expect(@dog1.age+@dog2.age).to eq 6
+    expect(@dog.age+@dog2.age).to eq 6
   end
 
   after(:all) do
-    puts "The first dog: #{@dog1.greeting}"
+    puts "The first dog: #{@dog.greeting}"
     puts "The second dog: #{@dog2.greeting}"
   end
 
